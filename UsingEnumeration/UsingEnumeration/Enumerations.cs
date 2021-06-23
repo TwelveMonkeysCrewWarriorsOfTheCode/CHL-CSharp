3b6ed634ce5e676e8e3696edb4d3c2556d8655b5
clambert-be <clambert.be@gmail.com>
2021-06-23 13:28:50 +0200

    Ajout Soulution Enumeration

---
+++ UsingEnumeration/UsingEnumeration/Enumerations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingEnumeration
+{
+    enum Color
+    {
+        Red,
+        Grenn,
+        Blue,
+        Yellow
+    }
+
+    enum JourSemaine
+    {
+        Lundi = 1,
+        Mardi,
+        Mercredi,
+        Jeudi,
+        Vendredi,
+        Samedi,
+        Dimanche
+    }
+
+    enum EtatCivil
+    {
+        Célibataire = 18,
+        Marié = 25,
+        Divorcé = 32,
+        Veuf = 45,
+        Concubin = 25
+    }
+}
+
+
