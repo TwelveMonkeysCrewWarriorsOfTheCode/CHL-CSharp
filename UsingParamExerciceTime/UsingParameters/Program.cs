9a07f5c3dbecb595c637ade624031cf962a81e94
imogwai <clambert.be@gmail.com>
2021-06-25 19:37:00 +0200

    Add UsingParamExerciceTime

---
+++ UsingParamExerciceTime/UsingParameters/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingParameters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Time> tempsTravFormate = new List<Time>();
+			List<string> tempsTravail = new List<string>();
+			for (int i = 0; i < 2; i++)
+			{
+				while (true)
+				{
+                    try
+                    {
+                        Console.Write($"Entrez un temps de travail {i + 1} : (HH:MM) ");
+                        string heurelue = Console.ReadLine();
+                        tempsTravail.Add(heurelue);
+                        Time travaille = new Time(heurelue);
+                        tempsTravFormate.Add(travaille);
+                        if (tempsTravFormate[i].Hour == -1)
+                        {
+                            Console.WriteLine("Erreur de format");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Out of Range");
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("La longueur du temps est fausse");
+                    }
+                    catch (Exception exc)
+                    {
+                        Console.WriteLine("Vous avez une erreur inconnue");
+                        Console.WriteLine(exc.Message);
+                    }
+                    
+				}
+			}
+
+			Time timeResult = Time.AddTime(tempsTravFormate[0], tempsTravFormate[1]);
+			Console.WriteLine(timeResult.ToString());
+		}
+	}
+}
+/* exercices :
+ * Poser la question à User
+ * Entrez un temps de travail :
+ * Quel jour avez-vous travaillé ?
+ * 1. Lundi
+ * 2. Mardi
+ * 3. Mercredi
+ * 4. Jeudi
+ * 5. Vendredi
+ * Avez-vous entré toutes vos prestations ? 
+ * (Enter pour continuer / O pour afficher le rapport)
+ * Add Time correct pour les minutes
+ * Soustraire un time
+ * Multiplier
+ * Diviser 
+ */
---
+++ UsingParamExerciceTime/UsingParameters/Time.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingParameters
+{
+    public class Time
+    {
+        const char SEPARATEUR_HEURE_MINUTE = (char)58; // :
+        const int NOMBRE_MAXIMUM_DE_MINUTES = 59;
+        public int Hour;
+        public int Minute;
+
+        public Time() // Constructeur : pas de paramétres et nom de la classe (constructeur par défault)
+        {
+            Hour = 0;
+            Minute = 0;
+        }
+
+        public Time(string pTemps)
+        {
+            int heurTravail, minuTravail;
+            // Tests format
+            // Test de garde (précondition)
+            if (pTemps.Length != 5) throw new ArgumentException();
+            char separateur = pTemps[2];
+            if (separateur != SEPARATEUR_HEURE_MINUTE) throw new ArgumentOutOfRangeException();
+            string[] detailTemps = pTemps.Split(SEPARATEUR_HEURE_MINUTE);
+            if (detailTemps.Length != 2) throw new ArgumentOutOfRangeException();
+            if (!int.TryParse(detailTemps[0], out heurTravail)) throw new ArgumentOutOfRangeException("heure", detailTemps[0], "Les heures ne sont pas dans un bon format");
+            if (!int.TryParse(detailTemps[1], out minuTravail)) throw new ArgumentOutOfRangeException("minute", detailTemps[1], "Les minutes ne sont pas dans un bon format");
+            // Fin des tests de garde (précondition)
+
+            AssignHourMinute(heurTravail, minuTravail);
+        }
+
+        public Time(int pHour, int pMinute)
+        {
+            AssignHourMinute(pHour, pMinute);
+        }
+
+        private void AssignHourMinute(int pHour, int pMinute)
+        {
+            if (pHour < 0 || pHour > 24 || pMinute < 0 || pMinute > NOMBRE_MAXIMUM_DE_MINUTES) throw new ArgumentOutOfRangeException();
+            this.Hour = pHour;
+            this.Minute = pMinute;
+        }
+
+        public override string ToString()
+        {
+            string s = $"{Hour}:{Minute} min.";
+            return s;
+        }
+
+        public static Time ChangerFormatEnTime(string pTempsTravail)
+        {
+            int heurTravail, minuTravail;
+            // Tests format
+            if (pTempsTravail.Length == 5)
+            {
+                heurTravail = int.Parse($"{pTempsTravail[0]}{pTempsTravail[1]}");
+                if ((heurTravail >= 0) && (heurTravail <= 24))
+                {
+                    minuTravail = int.Parse($"{pTempsTravail[3]}{pTempsTravail[4]}");
+                    if ((minuTravail >= 1) && (minuTravail <= 59))
+                    {
+                        char separateur = pTempsTravail[2];
+                        if (separateur == (char)58)
+                        {
+                            Time tempsFormat;
+                            tempsFormat = new Time();
+                            tempsFormat.Hour = heurTravail;
+                            tempsFormat.Minute = minuTravail;
+                            return tempsFormat;
+                        }
+                    }
+                }
+            }
+            Time erreurEntree;
+            erreurEntree = new Time();
+            erreurEntree.Hour = -1;
+            erreurEntree.Minute = -1;
+            return erreurEntree;
+        }
+
+
+        public static Time AddTime(Time pLeftTime, Time pRightTime)
+        {
+            Time mResultCorrige = new Time();
+            Time mResult = new Time();
+            mResult.Hour = pLeftTime.Hour + pRightTime.Hour;
+            mResult.Minute = pLeftTime.Minute + pRightTime.Minute;
+            mResultCorrige = ChangerMinutesEnHeures(mResult);
+            return mResultCorrige;
+        }
+
+        public static Time ChangerMinutesEnHeures(Time pTemps)
+        {
+            Time mResult = new Time();
+            int minutes = pTemps.Minute;
+            int heures = pTemps.Hour;
+            while (minutes >= 60)
+            {
+                minutes -= 60;
+                heures += 1;
+            }
+            mResult.Hour = heures;
+            mResult.Minute = minutes;
+            return mResult;
+        }
+    }
+
+}
