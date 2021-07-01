4107135db2f3501dce8d9ebd53541d13b9b5324f
clambert-be <clambert.be@gmail.com>
2021-07-01 11:35:26 +0200

    update une histoire

--- UneHistoire/UneHistoire/Program.cs
+++ UneHistoire/UneHistoire/Program.cs
@@ -7,6 +7,9 @@ namespace UneHistoire
         static void Main(string[] args)
         {
             Console.WriteLine("Début du programme");
+            Console.WriteLine("Quel est le type d'objet que vous voulez avertir ?");
+            string typeAvertissable = Console.ReadLine();
+            IAvertissable ia = IAvertissableFactory.Create(typeAvertissable,"18");
             //Personne b = new Boss("     Vador     ");
             //Boss b = new Boss("     Vador     ");
             //Personne b = new Ami("     Skywalker     ");
@@ -14,7 +17,8 @@ namespace UneHistoire
             IAvertissable b = new Wife("     Leia     ");
             //b.Prenom = "Princesse";
             //Console.WriteLine($"{b.Prenom}");
-            Worker w = new Worker(b); // Aussi une associatio de classe mais avec un constructeur
+            //Worker w = new Worker(b); // Aussi une associatio de classe mais avec un constructeur
+            Worker w = new Worker(ia);
             //w.Boss = b; // = Une association
             w.DoWork();
             Console.WriteLine("Fin du programme");
---
+++ UneHistoire/UneHistoire/Radiator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UneHistoire
+{
+    public class Radiator : IAvertissable
+    {
+        protected string m_Number;
+
+        public Radiator(string pNumber)
+        {
+            if (pNumber == null) throw new ArgumentNullException();
+            if (string.IsNullOrWhiteSpace(pNumber)) throw new ArgumentOutOfRangeException();
+            this.m_Number = pNumber.Trim();
+        }
+
+        public int WorkCompleted()
+        {
+            Console.WriteLine($"Radiator {m_Number} dit : Je suis réparé");
+            return 10; //grade
+        }
+
+        public void WorkProgressing()
+        {
+            Console.WriteLine($"Radiator {m_Number} dit : Je suis en train d'être réparé");
+        }
+
+        public void WorkStarted()
+        {
+            Console.WriteLine($"Radiator {m_Number} dit : Je commence à être réparé");
+        }
+    }
+}
